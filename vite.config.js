be716e8e3b28c2dc84859818511b0d2015e72220
anurag singh <anurag_singh20@outlook.com>
2026-09-06 21:59:07 +0530

    last commit

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,12 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    host: true,
+    allowedHosts: true,
+  },
+  preview: {
+    host: true,
+    allowedHosts: true,
+  },
 })
